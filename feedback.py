fa3aa173e4c2c86b47d7607355437a3f2dd01ff8
hoseinoori <hosein86noori@gmail.com>
2026-06-12 22:31:20 +0330

    Setting up and creating a database table

--- feedback.py
+++ feedback.py
@@ -3,3 +3,19 @@ from fastapi.middleware.cors import CORSMiddleware
 from sqlalchemy import create_engine, Column, Integer, String
 from sqlalchemy.orm import declarative_base, sessionmaker, Session
 from pydantic import BaseModel
+
+engine = create_engine("sqlite:///./feedback.db", connect_args={"check_same_thread": False})
+
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+Base = declarative_base()
+
+class FeedbackDB(Base):
+    __tablename__ = "feedbacks"
+    
+    id = Column(Integer, primary_key=True, index=True)
+    title = Column(String, index=True)
+    message = Column(String)
+    status = Column(String, default="ثبت شده")
+
+Base.metadata.create_all(bind=engine)
