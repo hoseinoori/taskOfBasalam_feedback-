1c874a4d28f7cdf92a1a42a9f633a982838bded1
hoseinoori <hosein86noori@gmail.com>
2026-06-12 21:24:07 +0330

    start

---
+++ feedback.py
@@ -0,0 +1,5 @@
+from fastapi import FastAPI, Depends, HTTPException
+from fastapi.middleware.cors import CORSMiddleware
+from sqlalchemy import create_engine, Column, Integer, String
+from sqlalchemy.orm import declarative_base, sessionmaker, Session
+from pydantic import BaseModel
