fda0ac73680d1371548478102a7caf0b3a26168f
hoseinoori <hosein86noori@gmail.com>
2026-06-13 04:02:38 +0330

    arrange

--- feedback.py
+++ feedback.py
@@ -4,28 +4,35 @@ from sqlalchemy import create_engine, Column, Integer, String
 from sqlalchemy.orm import declarative_base, sessionmaker, Session
 from pydantic import BaseModel
 
-engine = create_engine("sqlite:///./feedback.db", connect_args={"check_same_thread": False})
+engine = create_engine(
+    "sqlite:///./feedback.db", connect_args={"check_same_thread": False}
+)
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
 
+
 class FeedbackDB(Base):
     __tablename__ = "feedbacks"
-    
+
     id = Column(Integer, primary_key=True, index=True)
     title = Column(String, index=True)
     message = Column(String)
     status = Column(String, default="ثبت شده")
 
+
 Base.metadata.create_all(bind=engine)
+
 
 class FeedbackCreate(BaseModel):
     title: str
     message: str
 
+
 class FeedbackUpdate(BaseModel):
     status: str
+
 
 class FeedbackOut(BaseModel):
     id: int
@@ -35,7 +42,8 @@ class FeedbackOut(BaseModel):
 
     class Config:
         from_attributes = True
-        
+
+
 app = FastAPI(title="Feedback For Basalam(hoseinoori)")
 
 app.add_middleware(
@@ -46,13 +54,15 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
+
 def get_db():
     db = SessionLocal()
     try:
         yield db
     finally:
         db.close()
-        
+
+
 @app.post("/api/feedbacks/", response_model=FeedbackOut)
 def create_feedback(feedback: FeedbackCreate, db: Session = Depends(get_db)):
     new_feedback = FeedbackDB(title=feedback.title, message=feedback.message)
@@ -61,21 +71,25 @@ def create_feedback(feedback: FeedbackCreate, db: Session = Depends(get_db)):
     db.refresh(new_feedback)
     return new_feedback
 
+
 @app.get("/api/feedbacks/", response_model=list[FeedbackOut])
 def get_feedbacks(db: Session = Depends(get_db)):
     return db.query(FeedbackDB).all()
 
+
 @app.patch("/api/feedbacks/{feedback_id}/status", response_model=FeedbackOut)
-def update_feedback_status(feedback_id: int, status_update: FeedbackUpdate, db: Session = Depends(get_db)):
-    
+def update_feedback_status(
+    feedback_id: int, status_update: FeedbackUpdate, db: Session = Depends(get_db)
+):
+
     valid_statuses = ["ثبت شده", "در حال بررسی", "رسیدگی شده"]
     if status_update.status not in valid_statuses:
         raise HTTPException(status_code=400, detail="وضعیت نامعتبر است")
-    
+
     db_feedback = db.query(FeedbackDB).filter(FeedbackDB.id == feedback_id)
     if not db_feedback:
         raise HTTPException(status_code=404, detail="فیدبک پیدا نشد")
-    
+
     db_feedback.status = status_update.status
     db.commit()
     db.refresh(db_feedback)
